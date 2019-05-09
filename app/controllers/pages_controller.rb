3b44405d5a22c3e2a463652dd43541554c6114ea
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-09 14:42:27 +0100

    first rails app complete

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def form; end
+
+  def answer
+    @question = params[:question]
+
+    if @question == 'I am going to work right now!'
+      @answer = 'Great'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+   @answer
+  end
+end
